926e458a6dc9611bc96ddd2b91916728bef029f9
Annika Wassmo <annikaa@live.no>
2021-03-19 11:09:00 +0100

    Table component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./sass/style.scss"
-import Footer from "./components/Footer"
+import "./sass/style.scss";
+import Footer from "./components/Footer";
+import Table from "./components/Table";
 
 function App() {
   return (
     <>
     <div className="wrapper">
       <div className="container">
-
+        <Table />
       </div>
     </div>
     <Footer />
---
+++ src/components/Table.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Table() {
+    return (
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>2</th>
+      <th>4</th>
+      <th>6</th>
+      <th>10</th>
+      <th>14</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>name</td>
+      <td>Libero</td>
+      <td>Tincidut</td>
+      <td>Ullamcorper</td>
+      <td>Pretium</td>
+      <td>Tellus</td>
+    </tr>
+    <tr>
+      <td>Email</td>
+      <td>Tincidut@email.com</td>
+      <td>Ullamcorper@email.com</td>
+      <td>Pretium@email.com</td>
+      <td>Libero@email.com</td>
+      <td>Tellus@email.com</td>
+    </tr>
+    <tr>
+      <td>City</td>
+      <td>Oslo</td>
+      <td>Bergen</td>
+      <td>Oslo</td>
+      <td>Bergen</td>
+      <td>Oslo</td>
+    </tr>
+    <tr>
+      <td>Speciality</td>
+      <td>Cows</td>
+      <td>Mud</td>
+      <td>Sand Castles</td>
+      <td>Puppets</td>
+      <td>Donkeys</td>
+    </tr>
+  </tbody>
+</Table>
+    )
+}
+export default Table;
